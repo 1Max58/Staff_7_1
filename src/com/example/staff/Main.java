54aef4b47cd5f4283e2f83c6fbb082be83e2c934
1Max58 <>
2023-11-15 20:30:28 +0200

    Main class implemented

---
+++ src/com/example/staff/Main.java
@@ -0,0 +1,12 @@
+package com.example.staff;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("The program uses inheritance process - the Staff class is inherited from the base class Person");
+        Staff staffKorsun64068 = new Staff();
+        System.out.println("Please enter a staff");
+        staffKorsun64068.initialize1Korsun64068();
+        System.out.println("You entered the data:");
+        staffKorsun64068.print1Korsun64068();
+    }
+}
