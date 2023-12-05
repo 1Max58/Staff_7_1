7edf99c67e8697f6a07fe5c7c10d974543768571
1Max58 <>
2023-12-05 20:42:09 +0200

    salary for overtime calculation added

--- src/com/example/staff/Salary.java
+++ src/com/example/staff/Salary.java
@@ -3,7 +3,7 @@ package com.example.staff;
 public interface Salary {
     public double salaryToPayKorsun64068(double hours, double rate);
     public default double salaryForOvertimeKorsun64068(double hours, double rate) {
-        return 0;
+        return salaryToPayKorsun64068(hours, rate) * 2;
     }
     public static boolean shouldReceiveBonusKorsun64068(int yearsOfExpreience) {
         return yearsOfExpreience >= 10;
