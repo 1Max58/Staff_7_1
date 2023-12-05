e67b66ea1bd8cede27fbc2e9ddedd5b2d5998abd
1Max58 <>
2023-12-05 21:17:57 +0200

    Salary interface fixed

--- src/com/example/staff/Salary.java
+++ src/com/example/staff/Salary.java
@@ -1,11 +1,11 @@
 package com.example.staff;
 
 public interface Salary {
-    public double salaryToPayKorsun64068(double hours, double rate);
-    public default double salaryForOvertimeKorsun64068(double hours, double rate) {
+    double salaryToPayKorsun64068(double hours, double rate);
+    default double salaryForOvertimeKorsun64068(double hours, double rate) {
         return salaryToPayKorsun64068(hours, rate) * 2;
     }
-    public static boolean shouldReceiveBonusKorsun64068(int yearsOfExpreience) {
+    static boolean shouldReceiveBonusKorsun64068(int yearsOfExpreience) {
         return yearsOfExpreience >= 10;
     }
 }
